6bed42bb9a69e55e3968dc16a5de9401f344b309
rehmanarifin <rehmanarifin@gmail.com>
2025-09-29 02:33:25 +0600

    Add files via upload
    I have done sum,ads,multi

---
+++ 7th.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,abs;
+    printf("please enter an interger number = ");
+    scanf("%d",&num1);
+
+    printf("please enter an interger number = ");
+    scanf("%d",&num2);
+    abs=num1-num2;
+    printf("the abs is = %d",abs);
+}
---
+++ 8th.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,multi;
+    printf("the integer number = \n ");
+    scanf("%d",&num1);
+
+    printf("the integer number = \n");
+    scanf("%d",&num2);
+
+    multi=num1*num2;
+    printf("the multiplication is = %d \n",multi);
+
+    return 0;
+
+
+
+}
